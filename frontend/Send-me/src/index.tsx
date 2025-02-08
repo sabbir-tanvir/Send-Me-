1959579af4e48f2f381da2340ab240626f628dae
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2025-02-08 19:58:43 +0600

    final final

--- frontend/Send-me/src/index.tsx
+++ frontend/Send-me/src/index.tsx
@@ -1,5 +1,21 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; // Assuming you have an App component
+import App from './App.tsx'; // Assuming you have an App component
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  // Create a root
+  const root = ReactDOM.createRoot(rootElement);
+
+  // Render the App component
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
