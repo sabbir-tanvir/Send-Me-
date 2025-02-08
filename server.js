cd2843e0206efebb9c217e5fc272db0fdbf26120
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2025-02-08 21:52:12 +0600

    final 2.1

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post(
       user,
       200,
       res,
-      `User created successfully with the:- ${username}`
+      `Use this username & ur pass to Login:-  ${username}`
     );
   })
 );
